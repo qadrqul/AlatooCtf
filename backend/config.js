e0eec52fd1d319e63d64e8c399b6f6b7e15e67d5
Ya903040 <asirov560@gmail.com>
2023-05-17 09:58:49 +0600

    first commit

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 const dbHost = process.env.DB_HOST || '127.0.0.1';
-let dbUrl = 'mongodb://' + dbHost + '/ctf'
+let dbUrl = 'mongodb+srv://root:XDM6u4UpC2zZFrt@clusterforeducation.ufndjnt.mongodb.net/ctf'
+//let dbUrl = 'mongodb://' + dbHost + '/ctf'
 
 const rootPath = __dirname;
 
